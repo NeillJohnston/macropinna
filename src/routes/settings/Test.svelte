14fb94334e15552fd81e595e21f775af8402bf69
Neill Johnston <neilljohnston30@gmail.com>
2023-11-02 13:17:21 -0400

    Menu sections? (Little bit WIP)

--- src/routes/settings/Test.svelte
+++ src/routes/settings/Test.svelte
@@ -5,6 +5,7 @@
 	import Icon from "@iconify/svelte";
 	import Button from "../ui/Button.svelte";
 	import Checkbox from "../ui/Checkbox.svelte";
+	import MenuSection from "../ui/MenuSection.svelte";
 
     // TODO remove this. It's just a testing ground for UI.
 
@@ -12,60 +13,94 @@
     });
 
     let carouselIndex = 0;
+    let carousel2Index = 0;
     const carouselValues = [
         'Foo',
         'Bar but with a long name',
         'Baz'
-    ]
+    ];
 
     let checkboxOn = false;
+    let checkbox2On = false;
 </script>
 
 <div id="test">
-    <CarouselSelector
-        id='test/carousel'
-        component={{
-            down: { id: 'test/button' },
-            exit: {}
-        }}
-        bind:index={carouselIndex}
-        values={carouselValues}
-    />
+    <MenuSection label='Carousels'>
+        <CarouselSelector
+            id='test/carousel'
+            component={{
+                down: { id: 'test/carousel2' },
+                exit: {}
+            }}
+            bind:index={carouselIndex}
+            values={carouselValues}
+        />
+        <div class="space" />
+        <CarouselSelector
+            id='test/carousel2'
+            component={{
+                up: { id: 'test/carousel' },
+                down: { id: 'test/button' },
+                exit: {}
+            }}
+            bind:index={carousel2Index}
+            values={carouselValues}
+        />
+    </MenuSection>
     <div class="space" />
-    <Button
-        id='test/button'
-        component={{
-            up: { id: 'test/carousel' },
-            down: { id: 'test/button2' },
-            exit: {}
-        }}
-        onPress={() => {}}
-    >
-        Button
-    </Button>
+    <MenuSection label='Buttons'>
+        <Button
+            id='test/button'
+            component={{
+                up: { id: 'test/carousel2' },
+                down: { id: 'test/button2' },
+                exit: {}
+            }}
+            onPress={() => {}}
+        >
+            Button
+        </Button>
+        <div class="space" />
+        <Button
+            id='test/button2'
+            component={{
+                up: { id: 'test/button' },
+                down: { id: 'test/checkbox' },
+                exit: {}
+            }}
+            onPress={() => {}}
+        >
+            Another Button
+        </Button>
+    </MenuSection>
     <div class="space" />
-    <Button
-        id='test/button2'
-        component={{
-            up: { id: 'test/button' },
-            down: { id: 'test/checkbox' },
-            exit: {}
-        }}
-        onPress={() => {}}
-    >
-        Another Button
-    </Button>
-    <div class="space" />
-    <Checkbox
-        id='test/checkbox'
-        bind:on={checkboxOn}
-        component={{
-            up: { id: 'test/button2' },
-            exit: {}
-        }}
-    >
-        Checkbox
-    </Checkbox>
+    <MenuSection label='Checkboxes'>
+        <div style:display="flex">
+            <Checkbox
+                id='test/checkbox'
+                bind:on={checkboxOn}
+                component={{
+                    up: { id: 'test/button2' },
+                    right: { id: 'test/checkbox2' },
+                    exit: {}
+                }}
+            >
+                Checkbox
+            </Checkbox>
+            <div class="hspace" />
+            <Checkbox
+                id='test/checkbox2'
+                bind:on={checkbox2On}
+                component={{
+                    up: { id: 'test/button2' },
+                    left: { id: 'test/checkbox' },
+                    exit: {}
+                }}
+            >
+                Another Checkbox
+            </Checkbox>
+        </div>
+    </MenuSection>
 </div>
 
 <style>
@@ -76,5 +111,9 @@
 
     .space {
         height: var(--lg);
+    }
+
+    .hspace {
+        width: var(--lg);
     }
 </style>
---
+++ src/routes/ui/MenuSection.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    export let label: string;
+</script>
+
+<div id="menu-section">
+    <p id="label"><strong>{label}</strong></p>
+    <div id="menu">
+        <slot />
+    </div>
+</div>
+
+<style>
+    #menu-section {}
+
+    #label {
+        font-size: 0.71rem;
+    }
+
+    #menu {
+        padding-left: var(--lg);
+    }
+</style>
